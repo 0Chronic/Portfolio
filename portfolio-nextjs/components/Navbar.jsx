5febc56aaff1b071eccc8e183067ff9077b1c75b
0Chronic <kyawn1063@gmail.com>
2024-10-22 21:34:09 +0630

    day2broke

--- portfolio-nextjs/components/Navbar.jsx
+++ portfolio-nextjs/components/Navbar.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='fiexd w-full h-20 shadow-xl z-[100]'>
+      <div className='flex justify-between items-center w-full h-full px-2'>
+
+      </div>
+      </div>
   )
 }
 
